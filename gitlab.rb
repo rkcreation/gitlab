1800cc61ed290e271472aa14c39e67d752905ece
Nicolas Dhers <nicolas@rkcreation.fr>
2019-02-01 20:14:45 +0100

    remove https / letsencrypt from gitlab

--- gitlab.rb
+++ gitlab.rb
@@ -2,3 +2,7 @@ external_url "https://" + ENV['DOMAINS'] + "/"
 gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password')
 gitlab_rails['lfs_enabled'] = true
 gitlab_rails['gitlab_shell_ssh_port'] = 9022
+letsencrypt['enable'] = false
+nginx['listen_port'] = 80
+nginx['listen_https'] = false
+nginx['proxy_set_headers'] = {  "X-Forwarded-Proto" => "https", "X-Forwarded-Ssl" => "on" }
