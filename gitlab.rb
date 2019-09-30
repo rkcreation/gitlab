9775a661a06ac83b39672d999976746989d71d42
Nicolas Dhers <nicolas@rkcreation.fr>
2019-09-30 10:02:17 +0200

    working config v1

--- gitlab.rb
+++ gitlab.rb
@@ -2,14 +2,11 @@ external_url "https://" + ENV['DOMAIN']
 registry_external_url "https://" + ( ENV.has_key?('DOMAIN_REGISTRY') ? ENV['DOMAIN_REGISTRY'] : ( 'registry.' + ENV['DOMAIN'] ) )
 pages_external_url "https://" + ( ENV.has_key?('DOMAIN_PAGES') ? ENV['DOMAIN_PAGES'] : ( 'pages.' + ENV['DOMAIN'] ) )
 
-print File.read('/run/secrets/gitlab_smtp_password').strip
-
 gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password').strip
 gitlab_rails['lfs_enabled'] = true
 gitlab_rails['gitlab_shell_ssh_port'] = ENV.has_key?('GITLAB_SSH_PORT') ? ENV['GITLAB_SSH_PORT'] : 9022
 
 letsencrypt['enable'] = false
-print "ok l13"
 
 nginx['listen_port'] = 80
 nginx['listen_https'] = false
@@ -22,7 +19,6 @@ pages_nginx['listen_port'] = 5200
 pages_nginx['listen_https'] = false
 pages_nginx['proxy_set_headers'] = { "Host" => "$$http_host", "X-Real-IP" => "$$remote_addr", "X-Forwarded-For" => "$$proxy_add_x_forwarded_for", "X-Forwarded-Proto" => "https", "X-Forwarded-Ssl" => "on" }
 
-print "ok l26"
 gitlab_rails['smtp_enable'] = true
 gitlab_rails['smtp_address'] = ENV['SMTP_ADDRESS']
 gitlab_rails['smtp_port'] = ENV['SMTP_PORT']
@@ -37,11 +33,9 @@ gitlab_rails['smtp_openssl_verify_mode'] = ENV['SMTP_OPENSSL_VERIFY_MODE']
 gitlab_rails['gitlab_email_from'] = ENV['GITLAB_EMAIL']
 gitlab_rails['gitlab_email_reply_to'] = ENV['GITLAB_EMAIL_REPLY_TO'] || ENV['GITLAB_EMAIL']
 
-print "ok l41"
 gitlab_rails['backup_path'] = ( ENV['GITLAB_BAKUPS'] || '/backups' ) + '/gitlab'
 
 gitlab_rails['backup_keep_time'] = 604800
 
 gitlab_pages['inplace_chroot'] = true
 gitlab_pages['external_http'] = ['gitlab:5201']
-print "ok l48"
